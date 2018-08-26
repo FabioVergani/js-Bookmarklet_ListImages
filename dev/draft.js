4be04a13c22c5f170aade92e80247800666a5a72
Fabio Vergani <fabio.vergani.1975@gmail.com>
2018-08-26 19:33:06 +0200

    Update draft.js

--- dev/draft.js
+++ dev/draft.js
@@ -1,7 +1,9 @@
 (w=>{
 	if(w.ListImagesResult!==true){
 		(w.ListImages||(w.ListImages=(w=>{
-			const once=(w,s,f)=>{let e=w.document;if(e.readyState!=='complete'){w.addEventListener(s,e=o=>{w.removeEventListener(s,e);f(o)})}else{f({target:e})}},T=' \uD83D\uDDBC';
+			const once=(w,s,f)=>{let e=w.document;if(e.readyState!=='complete'){w.addEventListener(s,e=o=>{w.removeEventListener(s,e);f(o)})}else{f({target:e})}},
+			S='img,html{padding:0}body,html{margin:0;color:#7b7b7b}html{background:#f6f6f6 url(https:bit.ly/2nPgsph)}body{background:transparent;font:11px/13px sans-serif;text-align:center;padding:8px;user-select:none;}section:not(:first-child){margin-top:22px;border-top:2px dashed #e8e5e5}p{text-align:right;margin:4px 0}big{font-size:1.2em;vertical-align:text-bottom;color:#525252}b,img{cursor:pointer;color:#7c7b7b}img{transition:outline-color .2s ease-out,filter .5s ease-in;outline:2px outset #6a6a6a17}img{box-sizing:border-box;max-width:calc(100% - 16px);vertical-align:top;margin:8px;border:8px solid #fff;box-shadow:1px 1px 1px #d6d6d6,1px 1px 4px #d4d4d4;background:url(https://bit.ly/2vWcLT4)}img:hover{transition-timing-function:linear,linear;transition-duration:.1s;outline:2px solid #593db0;filter:brightness(1.05)}',
+			T=' \uD83D\uDDBC';
 			let opened,allowed=true;
 			return w=>{
 				if(allowed){
@@ -53,9 +55,7 @@
 									b=i.document.body;b.style.display='none';while(i=b.lastChild){i.remove()};
 									i=o=>{a(o.target)}
 								}else{
-									once(i=opened=w.open(),'DOMContentLoaded',o=>{
-										const d=o.target;
-									});
+									once(i=opened=w.open(),'DOMContentLoaded',o=>{c(o.target.head,'style').innerText=S});
 									i.addEventListener('beforeunload',()=>{opened=null});
 									i.ListImagesResult=true;
 									i=o=>{
@@ -72,15 +72,7 @@
 													j=--d.body.main.total;
 													d.title=(j!==0?j+T:'');
 												}else{
-													s=e.w;
-													if(s && s.closed!==true){
-														s.focus()
-													}else{
-														once(s=e.w=w.open(e.src||''),'DOMContentLoaded',o=>{
-															const d=o.target,b=d.body;
-														});
-														s.addEventListener('beforeunload',()=>{e.w=null})
-													}
+													s=e.w;if(s && s.closed!==true){s.focus()}else{once(e.w=w.open(e.src),'DOMContentLoaded',o=>{o.target.defaultView.addEventListener('beforeunload',()=>{e.w=null})})}
 												}
 											}
 										});
